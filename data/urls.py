0759d36b2ead82ddfbac0871deb2dbfe4681d4a3
grizzlydevil <53172842+grizzlydevil@users.noreply.github.com>
2022-06-05 16:43:44 +0300

    Add delete corpus action

--- data/urls.py
+++ data/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from .views import CreateDeleteCorpusViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
-router.register('words', CreateDeleteCorpusViewSet, basename='corpus')
+router.register('', CreateDeleteCorpusViewSet, basename='corpus')
 
 urlpatterns = router.urls
--- data/views.py
+++ data/views.py
@@ -1,20 +1,24 @@
 from rest_framework import status
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.mixins import CreateModelMixin
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.settings import api_settings
 
 from .serializers import CorpusSerializer
+from .models import Corpus
 
 
-class CreateDeleteCorpusViewSet(
-    GenericViewSet, CreateModelMixin
-):
+class CreateDeleteCorpusViewSet(GenericViewSet):
     """A viewset to store and delete words to the corpus"""
 
     serializer_class = CorpusSerializer
 
-    def create(self, request):
+    @action(detail=False, methods=['post', 'delete'], url_path='words')
+    def create_delete_corpus(self, request):
+        if request.method == 'DELETE':
+            Corpus.objects.all().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
         words = request.data.get('words')
 
         if not words or len(words) == 0:
@@ -26,6 +30,11 @@ class CreateDeleteCorpusViewSet(
         self.perform_create(serializer)
         return Response(status=status.HTTP_201_CREATED)
 
-    @action(detail=False, methods=['delete'], url_path='')
-    def delete_corpus(self, request):
-        pass
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def get_success_headers(self, data):
+        try:
+            return {'Location': str(data[api_settings.URL_FIELD_NAME])}
+        except (TypeError, KeyError):
+            return {}
