eaef18d4ac36f0becd2474392cdb77c6232e4eb6
grizzlydevil <53172842+grizzlydevil@users.noreply.github.com>
2022-06-05 17:32:01 +0300

    Add anagrams app

--- anagram_api/settings.py
+++ anagram_api/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
 
-    'data'
+    'data',
+    'anagrams'
 ]
 
 MIDDLEWARE = [
--- anagram_api/urls.py
+++ anagram_api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('data.urls')),
+    path('anagrams/', include('anagrams.urls')),
 ]
