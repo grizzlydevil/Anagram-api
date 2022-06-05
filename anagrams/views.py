c938f2ef1cc73371db9144eb10ce2ae08e57b190
grizzlydevil <53172842+grizzlydevil@users.noreply.github.com>
2022-06-05 18:11:59 +0300

    Create list anagrams view

--- anagrams/views.py
+++ anagrams/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+from data.models import Corpus
+
+
+class ListAnagrams(ListAPIView):
+    """List specified word anagrams limits results with limit query param"""
+
+    def list(self, request):
+        pass
+
+    def get_queryset(self):
+        word = self.request
+        limit = self.request
+
+        hash = Corpus.get_hash(word)
+        letter_chain = Corpus.get_alphagram(word)
+
+        queryset = (
+            Corpus.objects
+            .filter(hash=hash)
+            .exclude(word=word)
+            .filter(alphagram_chain=letter_chain)
+        )
+
+        if limit:
+            queryset[:limit]
+
+        return queryset
