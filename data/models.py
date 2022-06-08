79aa35ab02a72256debbce9621bba2d9d4cfb3be
grizzlydevil <53172842+grizzlydevil@users.noreply.github.com>
2022-06-08 22:54:28 +0300

    Fix get hash

--- data/models.py
+++ data/models.py
@@ -13,7 +13,7 @@ class Corpus(models.Model):
         Create a hash to store in the database which would be unique to every
         anagram
         """
-        lowercase_word = word.lower()
+        lowercase_word = word.lower().replace('-', '')
 
         # every letter of a word is hashed as a bitwise number
         # every letter has 2 bits corresponding to the times this letter
@@ -24,7 +24,7 @@ class Corpus(models.Model):
         # However in the rare cases when there is for example 4 letters 'a' in
         # the word the word hash would be the same as letter 'b'
         # For this purpose I also hash the length of a word
-        length_of_word = len(word)
+        length_of_word = len(lowercase_word)
 
         # shiftig 6 bits to the left and adding word length
         # 6 bits lets hashing word up to 63 letters long. Which is plenty
