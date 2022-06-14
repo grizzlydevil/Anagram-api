544cf8f754f92ea9cf1c3a329086cd8858632ff7
grizzlydevil <53172842+grizzlydevil@users.noreply.github.com>
2022-06-14 22:50:58 +0300

    Tests for GetAnagramsView

--- data/tests.py
+++ data/tests.py
@@ -1,5 +1,3 @@
-import math
-
 from statistics import median
 
 from django.test import TestCase
@@ -166,3 +164,91 @@ class StatsTests(TestCase):
         words['words'].append(new_word)
 
         self.assertDictEqual(self.get_data_dict(words), response.data)
+
+
+class GetAnagramsTests(TestCase):
+    """Test get anagrams view"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+        # creating some fake data
+        # 2x 5 word anagrams - most popular
+        # 1x 4 word anagrams
+        # 3x 3 word anagrams
+        # a couple of other words
+        self.data = {
+            'words': [
+                # 5 word anagrams
+                'spear', 'spare', 'pears', 'parse', 'apres',
+                # 2nd 5 word anagrams
+                'players', 'parsley', 'parleys', 'replays', 'sparely',
+                # 4 word anagrams
+                'aery', 'eyra', 'yare', 'year',
+
+                # 3x 3 word anagrams
+                'ear', 'are', 'era',
+                'lapser', 'parles', 'pearls',
+                'manor', 'moran', 'roman',
+
+                # some random words
+                'am', 'ar', 'aero' 'thing', 'ding', 'foo', 'nausea', 'arm',
+                'ram', 'mona', 'morn'
+            ]
+        }
+        self.client.post(CREATE_DELETE_CORPUS_URL, data=self.data)
+
+    def test_find_most_popular_anagrams(self, sets_from_db=None):
+        """Get the most popular anagrams (2x 5 word anagrams)"""
+        if not sets_from_db:
+            response = self.client.get(GET_ANAGRAMS_URL)
+
+            sets_from_db = response.data['words_with_most_anagrams']
+
+        # check if two most popular sets of words were returned
+        self.assertEqual(len(sets_from_db), 2)
+
+        # check if both sets of words have 5 members each
+        self.assertEqual(len(sets_from_db[0]), 5)
+        self.assertEqual(len(sets_from_db[1]), 5)
+
+        # check if response contains the same anagrams
+        first_biggest_set = self.data['words'][:5]  # 5 letter words
+        second_biggest_set = self.data['words'][5:10]  # 7 letter words
+
+        self.assertEqual(
+            sorted(first_biggest_set),
+
+            sorted(sets_from_db[0])
+            if len(sets_from_db[0][0]) == 5
+            else sorted(sets_from_db[1])
+        )
+
+        self.assertEqual(
+            sorted(second_biggest_set),
+
+            sorted(sets_from_db[0])
+            if len(sets_from_db[0][0]) == 7
+            else sorted(sets_from_db[1])
+        )
+
+    def test_get_anagram_groups_with_size(self):
+        """Test size query param getting size >= *x* groups of anagrams"""
+        url = f"{reverse('data:get-anagrams')}?size=4"
+        response = self.client.get(url)
+
+        sets_from_db = response.data['words_with_most_anagrams']
+
+        # check if three most popular sets of words were returned
+        self.assertEqual(len(sets_from_db), 3)
+
+        # check if the first two sets are still the same
+        self.test_find_most_popular_anagrams(sets_from_db=sets_from_db[:2])
+
+        # check if the last set has 4 members
+        self.assertEqual(len(sets_from_db[2]), 4)
+
+        # check if response contains the same anagrams
+        set_of_words = self.data['words'][10:14]  # 5 letter words
+
+        self.assertEqual(sorted(set_of_words), sorted(sets_from_db[2]))
